c0a4401349f6b8f1843891bbb99c13ad23aed494
XtheOne <xtheone@huizedegroot.nl>
2017-12-22 14:16:28 +0100

    Ability to merge tiles with different size. Left to Right (horizontalTiling) needs to be checked!

--- c-sharp/ImageMerger/TileMerger.cs
+++ c-sharp/ImageMerger/TileMerger.cs
@@ -13,19 +13,8 @@
 
         public static Bitmap MergeBitmaps(List<Bitmap> listOfTileBitmaps, int columnCount, bool horizontalTiling)
         {
-            int width = 1;
-            int height = 1;
-            foreach (Bitmap sourceTile in listOfTileBitmaps)
-            {
-                if (sourceTile.Width > width)
-                {
-                    width = sourceTile.Width;
-                }
-                if (sourceTile.Height > height)
-                {
-                    height = sourceTile.Height;
-                }
-            }
+            int width = 0;
+            int height = 0;
             if (columnCount > listOfTileBitmaps.Count)
             {
                 columnCount = listOfTileBitmaps.Count;
@@ -36,19 +25,55 @@
                 int temp = rowCount;
                 rowCount = columnCount;
                 columnCount = temp;
-            }            
-            Bitmap image = new Bitmap(columnCount * width, rowCount * height, PixelFormat.Format32bppArgb);
+            }
+
+            int count = 1;
+            foreach (Bitmap sourceTile in listOfTileBitmaps)
+            {
+                if (count <= rowCount)
+                {
+                    height += sourceTile.Height;
+                }
+                if (count % rowCount == 0)
+                {
+                    width += sourceTile.Width;
+                }
+                count++;
+            }
+
+            Bitmap image = new Bitmap(width, height, PixelFormat.Format32bppArgb);
             Graphics graphics = Graphics.FromImage(image);
             SolidBrush brush = new SolidBrush(Color.FromArgb(0, 0xff, 240, 200));
             graphics.FillRectangle(brush, 0, 0, image.Width, image.Height);
             int n = 0;
+            int rowHeight = 0;
+            int colWidth = 0;
+            int prevcol = 0;
+            int prevrow = 0;
+            int prevHeight = 0;
+            int prevWidth = 0;
             if (horizontalTiling)
             {
                 foreach (Bitmap tileBitmap in listOfTileBitmaps)
                 {
                     int col = (int)Math.Floor((double)(((double)n) / ((double)columnCount)));
                     int row = n % columnCount;
-                    Point location = new Point(((row * width) + (width / 2)) - (tileBitmap.Width / 2), ((col * height) + (height / 2)) - (tileBitmap.Height / 2));
+                    if (col == 0)
+                    {
+                        rowHeight = 0;
+                        prevcol = col;
+                    }
+                    if (col != prevcol)
+                    {
+                        rowHeight += prevHeight;
+                        prevcol = col;
+                    }
+                    if (row != prevrow)
+                    {
+                        colWidth += prevWidth;
+                        prevrow = row;
+                    }
+                    Point location = new Point(colWidth, rowHeight);
                     Size size = new Size(tileBitmap.Width, tileBitmap.Height);
                     Graphics.FromImage(image).DrawImage(tileBitmap, new Rectangle(location, size));
                     n++;
@@ -60,10 +85,27 @@
                 {
                     int row = (int)Math.Floor((double)(((double)n) / ((double)rowCount)));
                     int col = n % rowCount;
-                    Point location = new Point(((row * width) + (width / 2)) - (tileBitmap.Width / 2), ((col * height) + (height / 2)) - (tileBitmap.Height / 2));
+                    if (col == 0)
+                    {
+                        rowHeight = 0;
+                        prevcol = col;
+                    }
+                    if (col != prevcol)
+                    {
+                        rowHeight += prevHeight;
+                        prevcol = col;
+                    }
+                    if (row != prevrow)
+                    {
+                        colWidth += prevWidth;
+                        prevrow = row;
+                    }
+                    Point location = new Point(colWidth, rowHeight);
                     Size size = new Size(tileBitmap.Width, tileBitmap.Height);
                     Graphics.FromImage(image).DrawImage(tileBitmap, new Rectangle(location, size));
                     n++;
+                    prevWidth = tileBitmap.Width;
+                    prevHeight = tileBitmap.Height;
                 }
             }
             return image;
